708065d35facea4188a63561df42232e88184afa
Ansh Mishra <ansh2003mishra@gmail.com>
2026-02-18 17:03:29 +0530

    Custom Global and Route Specific filter in API gateway

---
+++ api-gateway/src/main/java/com/example/api_gateway/filter/CustomRouteFilter.java
@@ -0,0 +1,24 @@
+package com.example.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import reactor.core.publisher.Mono;
+
+public class CustomRouteFilter extends AbstractGatewayFilterFactory<Object> {
+
+    public CustomRouteFilter(){
+        super(Object.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return ((exchange, chain) -> {
+            System.out.println("Route Filter: Before routing");
+            exchange.getRequest().mutate().header("X-Route-Header", "Added-By-gateway").build();
+            return chain.filter(exchange).then(Mono.fromRunnable(()-> {
+                System.out.println("Route filter: After routing");
+            }));
+        });
+    }
+
+}

16aa91d4320fe00c35e5bfc9f01df4410d24584b
Ansh Mishra <ansh2003mishra@gmail.com>
2026-02-24 16:59:22 +0530

    Bug fixes in Inventory service

--- inventory-service/src/main/java/com/marketplace/inventory_service/repository/ProcessedOrderRepo.java
+++ inventory-service/src/main/java/com/marketplace/inventory_service/repository/ProcessedOrderRepo.java
@@ -2,7 +2,9 @@ package com.marketplace.inventory_service.repository;
 
 import com.marketplace.inventory_service.entity.ProcessedOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProcessedOrderRepo extends JpaRepository<ProcessedOrder, Long> {
 
     Boolean existsByOrderId(String orderId);

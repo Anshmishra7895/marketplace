7dace7acd02823d2ae9bcdb60172ae9d4be206b8
Ansh Mishra <ansh2003mishra@gmail.com>
2026-02-24 17:19:45 +0530

    Bug fixes in product service

--- product-service/src/main/java/com/marketplace/product_service/repository/CategoryRepo.java
+++ product-service/src/main/java/com/marketplace/product_service/repository/CategoryRepo.java
@@ -2,9 +2,11 @@ package com.marketplace.product_service.repository;
 
 import com.marketplace.product_service.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
 
     List<Category> findByParentId(Long parentId);
--- product-service/src/main/java/com/marketplace/product_service/service/CategoryService.java
+++ product-service/src/main/java/com/marketplace/product_service/service/CategoryService.java
@@ -1,9 +1,11 @@
 package com.marketplace.product_service.service;
 
 import com.marketplace.product_service.dto.CategoryDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CategoryService {
 
     CategoryDto createCategory(CategoryDto categoryDto);
